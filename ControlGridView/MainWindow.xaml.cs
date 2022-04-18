804a92cb8941bc62658ea2084d55564d78b02b78
Gleni Perez Martinez <99214705+GleniPerez@users.noreply.github.com>
2022-04-17 19:06:55 -0500

    proyecto terminado

---
+++ ControlGridView/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlGridView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+       private ObservableCollection<Persona> personas=new ObservableCollection<Persona>();
+        private PersonasDaoObjects personasDao = new PersonasDaoObjects();
+        public MainWindow()
+        {
+            InitializeComponent();
+            personas = new ObservableCollection<Persona>(personasDao.listar());
+
+            ListViewAgenda.ItemsSource = personas;
+        }
+    }
+}
---
+++ ControlGridView/PersonasDaoObjects.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlGridView
+{
+    
+    class PersonasDaoObjects
+    {
+        private List<Persona> listaPersonas= new List<Persona>();
+
+        public PersonasDaoObjects()
+        {
+            Persona p1 = new Persona { Nombre = "Javier", Email = "javier@gmail.com", Genero = "Hombre", Telefono = "221523212" , ImagenGenero="hombre.png" };
+            Persona p2 = new Persona { Nombre = "Rebecca", Email = "rebeca@gmail.com", Genero = "Mujer", Telefono = "221523321", ImagenGenero = "mujer.png" };
+            listaPersonas.Add(p1);
+            listaPersonas.Add(p2);
+        }
+        public List<Persona>listar()
+        {
+            return listaPersonas;
+        }
+    }
+}
